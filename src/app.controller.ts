55ddfe53021a97d709d1e0203983ced2422df0c2
xbsheng <xxbsheng@gmail.com>
2024-05-11 21:19:34 +0800

    test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -11,7 +11,7 @@ export class AppController {
   }
 
   @Post()
-  postHello(): string {
-    return this.appService.getHello();
+  postHello() {
+    return this.appService.postHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,15 @@ export class AppService {
   getHello(): string {
     return 'Hello World!123';
   }
+  postHello() {
+    return {
+      message: 'Hello World!',
+      data: {
+        name: 'John Doe',
+        age: 25,
+        address: '123 Main St, New York, NY 10030',
+      },
+      status: 'success',
+    };
+  }
 }
